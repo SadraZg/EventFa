2bfcd9d4d1a745986cc36213d71a8695c15ae0f8
aranaghapour <aran.h1990@gmail.com>
2019-07-09 07:56:32 -0700

    Add login page (Login and Sign up) Using POST functions

--- event/views.py
+++ event/views.py
@@ -3,7 +3,10 @@ from .models import Event, JoinEvent
 from django.http import Http404
 from django.contrib.auth.views import login_required
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
+
+pass_salt = '@131@13551eded'
 
 
 # Create your views here.
@@ -24,3 +27,65 @@ def event(request, event_id):
     except Event.DoesNotExist:
         raise Http404("رویداد موردنظر وجود ندارد!")
     return render(request, 'events/detail.html', {'event': specific_event, 'user_status': user_status})
+
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+def validate_user(request):
+    result = ''
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=make_password(password, pass_salt))
+
+        print(user)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('/account/dashboard')
+            else:
+                result = 'user is suspended!'
+        else:
+            print("Someone tried to login and failed.")
+            print("They used Mail: {} and Password: {}".format(username, password))
+            result = 'username or password is wrong'
+    else:
+        result = 'please use POST method'
+
+    return render(request, 'registration/login.html', {'result': result})
+
+
+def register(request):
+    if request.method == 'POST':
+        mail = request.POST.get('mail')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        cellphone = request.POST.get('cellphone')
+
+        if mail and first_name and last_name and cellphone and username is None:
+            return None
+
+        user = User.objects.create_user(username, email=mail,
+                                        password=make_password(password, pass_salt),
+                                        first_name=first_name, last_name=last_name)
+        # user.date_joined = datetime.now()
+
+        user.save()
+        if user is not None:
+            login(request, user)
+            return redirect('/account/dashboard')
+        else:
+            result = 'user already exists'
+
+            # send an activation mail to user
+        return render(request, 'registration/login.html', {'result': result})
+
+
--- eventfa/urls.py
+++ eventfa/urls.py
@@ -2,8 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from event import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('events/', include('event.urls')),
+    path('registration/', include('django.contrib.auth.urls')),
+    path('login-user', views.validate_user, name='login'),
+    path('logout', views.logout_user, name='logout'),
+    path('register-user', views.register, name='sign-up')
 ]
